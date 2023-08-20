d6740a90b24153217fb4f3680fd4945bd43734fe
Bruno Almeida <brunosra@gmail.com>
2023-08-20 16:43:32 +0100

    Add board logic

--- src/components/MainGame.jsx
+++ src/components/MainGame.jsx
@@ -1,26 +1,41 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "./Card";
+import "./MainGame.css";
+import Spinner from "./Spinner";
 
 function MainGame() {
-  const [data, setData] = useState({ hits: [] });
+  const [data, setData] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
 
-  useEffect(async () => {
-    const timestamp = currentDate.getTime();
-    const result = await axios(
-      "https://hn.algolia.com/api/v1/search?query=redux"
-    );
+  let qty = 10;
 
-    setData(result.data);
-  });
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: `http://localhost:5200/${qty}`,
+    };
+    axios
+      .request(options)
+      .then(function (response) {
+        setData(response.data);
+      })
+      .catch(function (error) {
+        console.error(error.error);
+      });
+  }, []);
 
   return (
     <div id="main-game">
       <div className="board">
-        <Card
-          name="Apocalypse"
-          image="http://i.annihil.us/u/prod/marvel/i/mg/f/e0/526166076a1d0.jpg"
-        />
+        {data.length > 0 ? (
+          data.map((hero) => (
+            <Card key={hero.name} name={hero.name} image={hero.image} />
+          ))
+        ) : (
+          <Spinner />
+        )}
       </div>
     </div>
   );
--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -6,7 +6,6 @@ function MainMenu({ handleStartGame }) {
       <h1>X-Men Memory Game</h1>
       <h2>Welcome to the Game</h2>
       <button onClick={handleStartGame}>Play</button>
-      {import.meta.env.VITE_API_KEY}
     </div>
   );
 }
